95208d3aab7c1d6eba97cad1901b034f47ce9aa0
Viktor <Viktorsidenko0@gmail.com>
2022-06-25 16:53:46 +0700

    update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'  ? '/ leadhit-test /'  : '/'
+  publicPath: process.env.NODE_ENV === 'production'  ? '/leadhit-test/'  : '/'
 })
